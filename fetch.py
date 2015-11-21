c67640b9678b6f1d9737aac570ece6117c821523
selfup <boudinot.regis@yahoo.com>
2015-11-21 16:31:04 -0700

    No interrupt - code will run if server is down or if local internet is down as well

--- fetch.py
+++ fetch.py
@@ -1,37 +1,43 @@
 
 import requests, json, time, sys
-
 sys.path.insert(0, '/usr/lib/python2.7/bridge/')
-
 from bridgeclient import BridgeClient as bridgeclient
+from requests.exceptions import ConnectionError
 
 endpoint_url = "http://intelyzine.com/api/v1/classroom_as"
-
 count  = 0
-
 value = bridgeclient()
 
 while (count < 100):
-  if (requests.get(endpoint_url).status_code) != 200:
+  try:
+    if (requests.get(endpoint_url).status_code) != 200:
 
-    print "No Interwebz  "
-    print "Not 200 OK    "
+      print "Server is Down"
+      print "200 NOT OK :( "
 
-    value.put("teacher",("No Interwebz  "))
-    value.put("cohort",("Not 200 OK    "))
+      value.put("teacher",("No Interwebz  "))
+      value.put("cohort",("Not 200 OK    "))
 
-    time.sleep(10)
-  else:
-    r = requests.get(endpoint_url)
+      time.sleep(10)
+    else:
+      r = requests.get(endpoint_url)
 
-    teacher = r.json()['teacher']
-    cohort = r.json()['cohort']
+      teacher = r.json()['teacher']
+      cohort = r.json()['cohort']
 
-    print r.status_code
-    print teacher
-    print cohort
+      print r.status_code
+      print teacher
+      print cohort
 
-    value.put("teacher",(teacher))
-    value.put("cohort",(cohort))
+      value.put("teacher",(teacher))
+      value.put("cohort",(cohort))
 
-    time.sleep(10)
+      time.sleep(10)
+  except ConnectionError as e:
+      print "No Interwebz  "
+      print "Internet Down "
+
+      value.put("teacher",("No Interwebz  "))
+      value.put("cohort",("Internet Down "))
+
+      time.sleep(10)
--- fetch_old.py
+++ fetch_old.py
@@ -1,36 +1,42 @@
-mport requests, json, time, sys
-
+import requests, json, time, sys
 sys.path.insert(0, '/usr/lib/python2.7/')
-
 from bridge.bridgeclient import BridgeClient as bridgeclient
+from requests.exceptions import ConnectionError
 
 endpoint_url = "http://intelyzine.com/api/v1/classroom_as"
-
 count  = 0
-
 value = bridgeclient()
 
 while (count < 100):
-  if (requests.get(endpoint_url).status_code) != 200:
+  try:
+    if (requests.get(endpoint_url).status_code) != 200:
 
-    print "No Interwebz  "
-    print "Not 200 OK    "
+      print "Server is Down"
+      print "200 NOT OK :( "
 
-    value.put("teacher",("No Interwebz  "))
-    value.put("cohort",("Not 200 OK    "))
+      value.put("teacher",("No Interwebz  "))
+      value.put("cohort",("Not 200 OK    "))
 
-    time.sleep(10)
-  else:
-    r = requests.get(endpoint_url)
+      time.sleep(10)
+    else:
+      r = requests.get(endpoint_url)
 
-    teacher = r.json()['teacher']
-    cohort = r.json()['cohort']
+      teacher = r.json()['teacher']
+      cohort = r.json()['cohort']
 
-    print r.status_code
-    print teacher
-    print cohort
+      print r.status_code
+      print teacher
+      print cohort
 
-    value.put("teacher",(teacher))
-    value.put("cohort",(cohort))
+      value.put("teacher",(teacher))
+      value.put("cohort",(cohort))
 
-    time.sleep(10)
+      time.sleep(10)
+  except ConnectionError as e:
+      print "No Interwebz  "
+      print "Internet Down "
+
+      value.put("teacher",("No Interwebz  "))
+      value.put("cohort",("Internet Down "))
+
+      time.sleep(10)
