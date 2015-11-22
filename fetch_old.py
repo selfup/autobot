e129a64f819f8b0bf552251e03d934d3a0ef4585
selfup <boudinot.regis@yahoo.com>
2015-11-21 18:52:58 -0700

    more python like indentation

--- fetch_old.py
+++ fetch_old.py
@@ -8,35 +8,35 @@ count  = 0
 value = bridgeclient()
 
 while (count < 100):
-  try:
-    if (requests.get(endpoint_url).status_code) != 200:
+    try:
+        if (requests.get(endpoint_url).status_code) != 200:
 
-      print "Server is Down"
-      print "200 NOT OK :( "
+            print "Server is Down"
+            print "200 NOT OK :( "
 
-      value.put("teacher",("Server is Down"))
-      value.put("cohort",("200 NOT OK :( "))
+            value.put("teacher",("Server is Down"))
+            value.put("cohort",("200 NOT OK :( "))
 
-      time.sleep(10)
-    else:
-      r = requests.get(endpoint_url)
+            time.sleep(10)
+        else:
+            r = requests.get(endpoint_url)
 
-      teacher = r.json()['teacher']
-      cohort = r.json()['cohort']
+            teacher = r.json()['teacher']
+            cohort = r.json()['cohort']
 
-      print r.status_code
-      print teacher
-      print cohort
+            print r.status_code
+            print teacher
+            print cohort
 
-      value.put("teacher",(teacher))
-      value.put("cohort",(cohort))
+            value.put("teacher",(teacher))
+            value.put("cohort",(cohort))
 
-      time.sleep(10)
+            time.sleep(10)
   except ConnectionError as e:
-      print "No Interwebz  "
-      print "PANICMODE INIT"
+            print "No Interwebz  "
+            print "PANICMODE INIT"
 
-      value.put("teacher",("No Interwebz  "))
-      value.put("cohort",("PANICMODE INIT"))
+            value.put("teacher",("No Interwebz  "))
+            value.put("cohort",("PANICMODE INIT"))
 
-      time.sleep(10)
+            time.sleep(10)
